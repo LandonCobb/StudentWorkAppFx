ab0dabf1c44a1a839a3cc6b6b6a620ef40834af6
0xJest <EndlessJest@gmail.com>
2022-08-10 15:52:25 -0600

    First benchmark for Scheduler Widget GUI

--- src/main/java/com/example/studentworkappfx/BaseApplication.java
+++ src/main/java/com/example/studentworkappfx/BaseApplication.java
@@ -7,12 +7,14 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class BaseApplication extends Application {
+    //App Dimensions should be 900x630px
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(BaseApplication.class.getResource("Scheduler.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 630);
         stage.setTitle("Hello!");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/studentworkappfx/SchedulerController.java
@@ -0,0 +1,10 @@
+package com.example.studentworkappfx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+public class SchedulerController {
+
+}
