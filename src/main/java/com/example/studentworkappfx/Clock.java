45a1cb3349615325eeba09ad6c5c00fbfa1f776e
LandonCobb <landoncobb123@gmail.com>
2022-08-10 15:51:33 -0600

    finished a very basic GUI for clock and added properties to clock controller

--- src/main/java/com/example/studentworkappfx/Clock.java
+++ src/main/java/com/example/studentworkappfx/Clock.java
@@ -1,6 +1,19 @@
 package com.example.studentworkappfx;
 
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
 public class Clock {
+
+    @FXML
+    private Text clockText;
+
+    @FXML
+    private Button swButton;
+
+    @FXML
+    private Text swFinishText;
 
 }
