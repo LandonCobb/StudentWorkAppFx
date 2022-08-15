432491247a1200f5372990101bffebc3c5b7e389
IssacMorales <imorales@student.neumont.org>
2022-08-15 16:04:47 -0600

    Got notes working, you are able to make new notes and edit old ones

--- src/main/java/com/example/studentworkappfx/NoteController.java
+++ src/main/java/com/example/studentworkappfx/NoteController.java
@@ -1,14 +1,11 @@
 package com.example.studentworkappfx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.net.URL;
+import java.io.*;
+import java.util.Scanner;
 
 
 public class NoteController {
@@ -16,8 +13,47 @@ public class NoteController {
     private TextArea text;
 
     @FXML
-    protected void save() {
+    private TextField d;
+
+    private static String dd = "";
+    @FXML
+    protected void save() throws IOException {
         System.out.println(text.getText());
+        dd = d.getText();
+        writeToFile(text.getText());
+    }
+
+    public static void writeToFile(String existingLine) throws IOException {
+        PrintStream out = new PrintStream(new FileOutputStream( dd + ".txt", false));
+        try {
+            out.println(existingLine);
+        } finally {
+            //fileOut.flush();
+            out.close();
+        }
+    }
+
+    @FXML
+    protected void load() throws IOException {
+        System.out.println(text.getText());
+        dd = d.getText();
+        readFile();
+    }
+
+    public void readFile() throws IOException {
+        InputStream fileIn = new FileInputStream(dd + ".txt");
+        BufferedReader in = new BufferedReader(new InputStreamReader(fileIn));
+        String line = "";
+        try{
+            line = in.readLine();
+            while(line != null){
+                System.out.println(line);
+                text.setText(line);
+                line = in.readLine();
+            }
+        }finally {
+            fileIn.close();
+        }
     }
 
 }
