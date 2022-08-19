92f67a4a114d15e6f5cbb8a7629a42d8a10bb1a9
IssacMorales <imorales@student.neumont.org>
2022-08-19 15:42:00 -0600

    bruh

--- src/main/java/com/example/studentworkappfx/NoteController.java
+++ src/main/java/com/example/studentworkappfx/NoteController.java
@@ -28,7 +28,6 @@ public class NoteController {
         try {
             out.println(existingLine);
         } finally {
-            //fileOut.flush();
             out.close();
         }
     }
