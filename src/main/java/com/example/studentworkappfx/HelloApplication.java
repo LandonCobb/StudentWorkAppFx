6bef7a38b8d0a270250875f5e2c3dc2853def971
0xJest <EndlessJest@gmail.com>
2022-08-26 15:19:37 -0600

    Setup basic data for simple menu elements

--- src/main/java/com/example/studentworkappfx/HelloApplication.java
+++ src/main/java/com/example/studentworkappfx/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Scheduler.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 630);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Scheduler");
         stage.setResizable(false);
         stage.setScene(scene);
--- src/main/java/com/example/studentworkappfx/SchedulerController.java
+++ src/main/java/com/example/studentworkappfx/SchedulerController.java
@@ -5,29 +5,121 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 
 import java.net.URL;
 import java.time.LocalDate;
-import java.util.ResourceBundle;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class SchedulerController implements Initializable {
     @FXML private Text dPaneNumday;
     @FXML private Text dPaneWeekday;
     @FXML private Text txtChangeDate;
-    @FXML private Spinner spnScheduleYEAR;
-    @FXML private Spinner spnScheduleMONTH;
     @FXML private Spinner spnScheduleDAY;
+    @FXML private Spinner spnScheduleMONTH;
+    @FXML private Spinner spnScheduleYEAR;
     @FXML private Button btnSelectDate;
     @FXML private Button btnEditEvent;
     @FXML private Button btnAddEvent;
     @FXML private Button btnClearSchedule;
 
-    private void setup() {
+    @FXML private TableView<TimeTableEntry> timeTable;
+    @FXML private TableColumn<TimeTableEntry, String> timeTableTimes;
+    @FXML private TableColumn<TimeTableEntry, String> timeTableEvents;
+
+    private static class Month {
+        private final String name;
+        private final int days;
+
+        Month(String name, int days){
+            this.name = name;
+            this.days = days;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getDays() {
+            return days;
+        }
+    }
+
+    private final ObservableList<Month> months = FXCollections.observableArrayList(
+            new Month("January", 30),
+            new Month("February", 30),
+            new Month("March", 30),
+            new Month("April", 30),
+            new Month("May", 30),
+            new Month("June", 30),
+            new Month("July", 30),
+            new Month("August", 30),
+            new Month("September", 30),
+            new Month("October", 30),
+            new Month("November", 30),
+            new Month("December", 30)
+    );
+
+    private ObservableList<String> getMonthNames(ObservableList<Month> months) {
+        List<String> monthNames;
+        monthNames = new ArrayList<String>();
+
+        months.forEach(month -> {
+            System.out.println(month.getName());
+            monthNames.add(month.getName());
+        });
+
+        return FXCollections.observableList(monthNames);
+    }
+
+    private ObservableList<String> monthName = getMonthNames(months);
+
+    private final Map<String, Integer> monthInfo = new HashMap<>() {{
+        this.put("January", 31);
+        this.put("February", 28);
+        this.put("March", 31);
+        this.put("April", 30);
+        this.put("May", 31);
+        this.put("June", 30);
+        this.put("July", 31);
+        this.put("August", 31);
+        this.put("September", 30);
+        this.put("October", 31);
+        this.put("November", 30);
+        this.put("December", 31);
+    }};
+
+    private final ObservableList<TimeTableEntry> timeTableData = FXCollections.observableArrayList(
+            new TimeTableEntry("6:00", ""),
+            new TimeTableEntry("6:30", ""),
+            new TimeTableEntry("7:00", ""),
+            new TimeTableEntry("7:30", ""),
+            new TimeTableEntry("8:00", ""),
+            new TimeTableEntry("8:30", ""),
+            new TimeTableEntry("9:00", ""),
+            new TimeTableEntry("9:30", ""),
+            new TimeTableEntry("10:00", ""),
+            new TimeTableEntry("10:30", ""),
+            new TimeTableEntry("11:00", ""),
+            new TimeTableEntry("11:30", ""),
+            new TimeTableEntry("12:00", ""),
+            new TimeTableEntry("12:30", ""),
+            new TimeTableEntry("1:00", ""),
+            new TimeTableEntry("1:30", ""),
+            new TimeTableEntry("2:00", ""),
+            new TimeTableEntry("2:30", ""),
+            new TimeTableEntry("3:00", ""),
+            new TimeTableEntry("3:30", ""),
+            new TimeTableEntry("4:30", ""),
+            new TimeTableEntry("5:00", ""),
+            new TimeTableEntry("5:30", ""),
+            new TimeTableEntry("6:00", "")
+    );
+
+    private void setupDate() {
         LocalDate date = LocalDate.now();
         int day = date.getDayOfMonth();
         String month = String.valueOf(date.getMonth());
@@ -43,13 +135,7 @@ public class SchedulerController implements Initializable {
 
         // The following code block is attributed to an unnamed contributor from "o7planning".
         // Site link: https://o7planning.org/11185/javafx-spinner
-
-        ObservableList<String> months = FXCollections.observableArrayList(//
-                "January", "February", "March", "April", //
-                "May", "June", "July", "August", //
-                "September", "October", "November", "December");
-
-        SpinnerValueFactory<String> valueFactoryMonths = new SpinnerValueFactory.ListSpinnerValueFactory<String>(months);
+        SpinnerValueFactory<String> valueFactoryMonths = new SpinnerValueFactory.ListSpinnerValueFactory<String>(monthName);
         SpinnerValueFactory<Integer> valueFactoryDay = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 30, day);
         SpinnerValueFactory<Integer> valueFactoryYear = new SpinnerValueFactory.IntegerSpinnerValueFactory(year, year + 10, year);
         valueFactoryMonths.setValue(month);
@@ -58,17 +144,53 @@ public class SchedulerController implements Initializable {
         spnScheduleDAY.setValueFactory(valueFactoryDay);
         spnScheduleMONTH.setValueFactory(valueFactoryMonths);
         spnScheduleYEAR.setValueFactory(valueFactoryYear);
-
     }
 
-    private void initSpinner() {
+    private void setupTimeTable() {
+        timeTableTimes.setCellValueFactory(new PropertyValueFactory("time"));
+        timeTableEvents.setCellValueFactory(new PropertyValueFactory("event"));
+        timeTable.setItems(timeTableData);
+        //timeTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        //timeTable.getColumns().addAll(timeTableTimes, timeTableEvents);
 
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Platform.runLater(() -> {
-            setup();
+            setupDate();
+            setupTimeTable();
         });
     }
+
+    private void syncDate() {
+
+    }
+
+    @FXML private void onSelectDate() {
+        String day = spnScheduleDAY.getValue().toString();
+        String month = spnScheduleMONTH.getValue().toString();
+        String year = spnScheduleYEAR.getValue().toString();
+        String date = day + "/" + month + "/" + year;
+
+        System.out.println(date);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
+        LocalDate localDate = LocalDate.parse(date, formatter);
+        System.out.println("!" + localDate.toString());
+    }
+
+    @FXML private void onEditEvent() {
+
+    }
+
+    @FXML private void onAddEvent() {
+
+    }
+
+    @FXML private void onClearSchedule() {
+
+    }
+
+
+
 }
