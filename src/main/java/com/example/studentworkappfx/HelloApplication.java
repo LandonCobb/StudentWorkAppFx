52b1c40d7de0f04d73e986d1c1a8b7f6a5dbe7aa
IssacMorales <imorales@student.neumont.org>
2022-08-29 15:31:09 -0600

    Home is fully done

--- src/main/java/com/example/studentworkappfx/HelloApplication.java
+++ src/main/java/com/example/studentworkappfx/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Scheduler.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Scheduler");
         stage.setResizable(false);
